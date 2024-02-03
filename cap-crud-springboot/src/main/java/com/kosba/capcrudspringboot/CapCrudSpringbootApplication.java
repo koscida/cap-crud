8a0c4d56823ea2760c2e07c015425a7ca15c5e90
koscida <britkos@gmail.com>
2024-02-03 09:35:18 -0500

    first commit

---
+++ cap-crud-springboot/src/main/java/com/kosba/capcrudspringboot/CapCrudSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.kosba.capcrudspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapCrudSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapCrudSpringbootApplication.class, args);
+	}
+
+}
---
+++ cap-crud-springboot/src/test/java/com/kosba/capcrudspringboot/CapCrudSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosba.capcrudspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapCrudSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
