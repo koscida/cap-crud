7988ac8852aa8f04d048bcd2227d2764a9839c81
koscida <britkos@gmail.com>
2024-02-04 11:40:02 -0500

    add credit

--- cap-crud-springboot/src/main/java/com/kosba/capcrudspringboot/CapCrudSpringbootApplication.java
+++ cap-crud-springboot/src/main/java/com/kosba/capcrudspringboot/CapCrudSpringbootApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CapCrudSpringbootApplication {
 
+	// based on this tutorial:
+	// https://www.javaguides.net/2019/08/spring-boot-crud-rest-api-spring-data-jpa-h2-database-example.html
 	public static void main(String[] args) {
 		SpringApplication.run(CapCrudSpringbootApplication.class, args);
 	}
